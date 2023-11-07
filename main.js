bd2a93cebf93ecc1f97521b9ef40b80a12f991ca
skkhan5 <yousafzai5588@gmail.com>
2023-11-07 22:28:40 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+import inquirer from "inquirer";
+let conversation = {
+    "PKR": {
+        "USD": 0.0044,
+        "GBP": 0.8037,
+        "PKR": 1
+    },
+    "GBP": {
+        "USD": 1.21,
+        "GBP": 1271.79,
+        "PKR": 1
+    },
+    "USD": {
+        "PKR": 225.50,
+        "GBP": 0.83,
+        "USD": 1
+    }
+};
+const answer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["PKR", "USD", "GBP"],
+        message: "select your currency"
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "USD", "GBP"],
+        message: "select your corvertion currency"
+    },
+    {
+        type: "number",
+        name: "amount",
+        choices: ["PKR", "USD", "GBP"],
+        message: "enter your amount"
+    },
+]);
+const { from, to, amount } = answer;
+if (from && to && amount) {
+    let result = conversation[from][to] * amount;
+    console.log(`your convertion from ${from}to ${to} is ${result}`);
+}
+else {
+    console.log("invalid input");
+}
